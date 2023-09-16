75d60af092ab743521d39b2de37153d7d90b24e7
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-16 15:49:43 +0200

    hemisphereLight

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function main() {
   //
 
   const canvas = document.querySelector('#c')
-  const renderer = new THREE.WebGLRenderer({ antiaslias: true, canvas })
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
 
   const fov = 45
   const aspect = 2
@@ -19,33 +19,35 @@ function main() {
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
   camera.position.set(0, 10, 20)
 
-  const scene = new THREE.Scene()
-
   const controls = new OrbitControls(camera, canvas)
   controls.target.set(0, 5, 0)
   controls.update()
 
-  const planeSize = 40
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color('black')
 
-  const loader = new THREE.TextureLoader()
-  const texture = loader.load('resources/images/checker.png')
-  texture.wrapS = THREE.RepeatWrapping
-  texture.wrapT = THREE.RepeatWrapping
-  texture.magFilter = THREE.NearestFilter
-  texture.colorSpace = THREE.SRGBColorSpace
-  const repeats = planeSize / 2
-  texture.repeat.set(repeats, repeats)
+  {
+    const planeSize = 40
 
-  const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize)
-  const planeMat = new THREE.MeshPhongMaterial({
-    map: texture,
-    side: THREE.DoubleSide,
-  })
+    const loader = new THREE.TextureLoader()
+    const texture = loader.load('resources/images/checker.png')
+    texture.wrapS = THREE.RepeatWrapping
+    texture.wrapT = THREE.RepeatWrapping
+    texture.magFilter = THREE.NearestFilter
+    texture.colorSpace = THREE.SRGBColorSpace
+    const repeats = planeSize / 2
+    texture.repeat.set(repeats, repeats)
 
-  const mesh = new THREE.Mesh(planeGeo, planeMat)
-  mesh.rotation.x = Math.PI * -0.5
-  scene.add(mesh)
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize)
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    })
 
+    const mesh = new THREE.Mesh(planeGeo, planeMat)
+    mesh.rotation.x = Math.PI * -0.5
+    scene.add(mesh)
+  }
   {
     const cubeSize = 4
     const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize)
@@ -64,34 +66,47 @@ function main() {
       sphereHeightDivisions
     )
     const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' })
-
     const mesh = new THREE.Mesh(sphereGeo, sphereMat)
     mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0)
     scene.add(mesh)
   }
 
-  const color = 0xffffff
-  const intensity = 1
-  const light = new THREE.AmbientLight(color, intensity)
-  scene.add(light)
-
-  class ColorGuiHelper {
+  class ColorGUIHelper {
+    //
     constructor(object, prop) {
+      //
       this.object = object
       this.prop = prop
+      //
     }
     get value() {
+      //
       return `#${this.object[this.prop].getHexString()}`
+      //
     }
     set value(hexString) {
+      //
       this.object[this.prop].set(hexString)
+      //
     }
   }
-  const gui = new GUI()
-  gui.addColor(new ColorGuiHelper(light, 'color'), 'value').name('color')
-  gui.add(light, 'intensity', 0, 2, 0.01)
+  {
+    const skyColor = 0xb1e1ff // light blue
+    const groundColor = 0xb97a20 // brownish orange
+    const intensity = 1
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity)
+    scene.add(light)
+
+    const gui = new GUI()
+    gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('skyColor')
+    gui
+      .addColor(new ColorGUIHelper(light, 'groundColor'), 'value')
+      .name('groundColor')
+    gui.add(light, 'intensity', 0, 5, 0.01)
+  }
 
   function resizeRendererToDisplaySize(renderer) {
+    //
     const canvas = renderer.domElement
     const width = canvas.clientWidth
     const height = canvas.clientHeight
@@ -104,6 +119,7 @@ function main() {
   }
   function render() {
     if (resizeRendererToDisplaySize(renderer)) {
+      //
       const canvas = renderer.domElement
       camera.aspect = canvas.clientWidth / canvas.clientHeight
       camera.updateProjectionMatrix()
